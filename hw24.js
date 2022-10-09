931483159c54b16e5347d21e9125a64b7e6621ad
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-10-10 02:25:47 +0300

    commit

--- hw24.js
+++ hw24.js
@@ -28,12 +28,15 @@ console.log(arr5);
 
 // 6. Заполните массив следующим образом: в первый элемент запишите ‘1’, во второй ’22’, в третий ‘333’ и так далее.
 let arr6 = [];
-let item2 = ``;
-for (let i = 0; i < 4; i++) {
-    item2 += i;
-    arr6.push(Number(item2));
 
+for (let i = 1; i < 4; i++) {
+    let item2 = ``;
+    for (let j = 0; j < i; j++) {
+        item2 += i;
+    }
+    arr6.push(item2);
 }
+
 console.log(arr6);
 
 // 7. Сделайте функцию arrayFill, которая будет заполнять массив заданными значениями. Первым параметром функция принимает значение, которым заполнять массив, а вторым — сколько элементов должно быть в массиве. Пример: arrayFill(‘x’, 5) сделает массив [‘x’, ‘x’, ‘x’, ‘x’, ‘x’].
@@ -52,24 +55,48 @@ arrayFill(elem, fillCount);
 
 // 8. Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
 let arr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let arr8Counter = [];
+let arr8Counter = 0;
+let sum = 0;
 
 for (let i = 0; i < arr8.length; i++) {
-    arr8Counter += arr8Counter[i];
-
+    arr8Counter += arr8[i];
+    if (arr8Counter > 10) {
+        sum = i;
+        break;
+    }
 }
-console.log(arr8Counter);
+console.log(sum);
 
 // 9. Дан массив с числами. Не используя метода reverse переверните его элементы в обратном порядке.
 let arr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arr9result = [];
-a = ``;
-for (let i = 0; i < arr9.length; i++) {
-    a = arr9.pop()
-    arr9result.unshift(a);
+let a9 = ``;
+for (let i = arr9.length; i < arr9.length; i++) {
+    a9 = arr9.pop()
+    arr9result.unshift(a9);
 }
-console.log(arr9result);
+console.log(arr9);
 
 // 10. Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
-//
+let arr10 = [[1, 2, 3], [4, 5], [6]];
+let flatArr10 = arr10.flat();
+let sum10 = 0;
+for (let i = 0; i < flatArr10.length; i++) {
+    sum10 += flatArr10[i];
+}
+console.log(sum10);
+
 // 11. Дан трехмерный массив с числами, например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+let arr11 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let sum11 = 0;
+
+function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
+const flatArr11 = flatten(arr11);
+for (let i = 0; i < flatArr11.length; i++) {
+    sum11 += flatArr11[i];
+}
+console.log(sum11);
